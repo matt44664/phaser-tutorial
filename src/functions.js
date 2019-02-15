2674def365d147595f1b548fc372a316f8fb63bc
Matt44664 <dank_penguin@hotmail.com>
2019-02-15 18:30:34 +0000

    Project structure

---
+++ src/functions.js
@@ -0,0 +1,48 @@
+function collectStar(player, star) {
+    star.disableBody(true, true);
+
+    //  Add and update the score
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0) {
+        //  A new batch of stars to collect
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+
+    } // if
+}
+
+
+function collectkey(player, key) {
+    key.disableBody(true, true);
+
+    console.log('collected key');
+
+    //  Add and update the score
+    score += 50;
+    scoreText.setText('Score: ' + score);
+}
+
+
+
+function hitBomb(player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+}
